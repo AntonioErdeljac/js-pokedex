f1a38fa3535fa64b32a356f5ed311c291526c8a5
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-28 15:58:19 +0100

    feat(prettier): add prettier config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const App = () => <p>Hello app!</p>;
+const App = () => (
+  <p>
+    Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello
+    app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello app!Hello
+    app!Hello app!Hello app!Hello app!Hello app!Hello app!
+  </p>
+);
 
 export default App;
