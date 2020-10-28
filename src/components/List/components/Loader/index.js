1105727b756c9b7f96ff5a0591cc6eedfe2571af
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-29 00:38:49 +0100

    refactor(skeleton): modify skeleton theme

--- src/components/List/components/Loader/index.js
+++ src/components/List/components/Loader/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { SkeletonTheme } from 'react-loading-skeleton';
 
 import Styled from './styles';
 
 const Loader = () => {
   return (
-    <Container>
-      <Row>
-        {[...new Array(3)].map((index) => (
-          <Col md={4} className="mb-4">
-            <Styled.Skeleton height={450} key={index} />
-            <Styled.Skeleton height={30} className="mt-3" />
-            <Styled.Skeleton height={30} width={300} className="mt-3" />
-          </Col>
-        ))}
-      </Row>
-    </Container>
+    <SkeletonTheme color="#f3f3f3" highlightColor="#efefef">
+      <Container>
+        <Row>
+          {[...new Array(3)].map((index) => (
+            <Col md={4} className="mb-4">
+              <Styled.Skeleton height={380} key={index} />
+              <Styled.Skeleton height={30} className="mt-3" />
+              <Styled.Skeleton height={30} width={300} className="mt-3" />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </SkeletonTheme>
   );
 };
 
