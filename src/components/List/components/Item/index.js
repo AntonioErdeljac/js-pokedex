337d8f5a239f40bda3138dd73af96dc6b7c85930
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-29 02:10:31 +0100

    refactor(webpack): remove parcel in favor of webpack

--- src/components/List/components/Item/index.js
+++ src/components/List/components/Item/index.js
@@ -1,20 +1,23 @@
+import cn from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React, { memo } from 'react';
 import { Col, Card } from 'react-bootstrap';
 
-import Styled from './styles';
+import styles from './styles.scss';
 import { generateImage } from './helpers';
+
+const cx = cn.bind(styles);
 
 const Item = ({ name, index }) => {
   return (
-    <Col key={name} md={4}>
-      <Styled.Card className="mb-4 shadow-sm">
+    <Col className={cx('pd-card')} key={name} md={4}>
+      <Card className="mb-4 shadow-sm">
         <Card.Img className="p-5" variant="top" src={generateImage(index)} />
         <hr />
         <Card.Body>
           <Card.Title className="text-capitalize font-weight-bold text-center">{name}</Card.Title>
         </Card.Body>
-      </Styled.Card>
+      </Card>
     </Col>
   );
 };
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: '',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/index.html`,
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    open: true,
+  },
+};
