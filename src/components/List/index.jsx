a64443bbf5f4f5cf34f1bbd947e0cb405156b1e9
Antonio Erdeljac <hedhogs@gmail.com>
2020-10-30 12:21:11 +0100

    feat(animations): add basic animations

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,6 +2,7 @@ import cn from 'classnames/bind';
 import PropTypes from 'prop-types';
 import React from 'react';
 import Visibility from 'react-visibility-sensor';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Container, Row } from 'react-bootstrap';
 import { isEmpty } from 'lodash';
 
@@ -28,18 +29,26 @@ const List = ({ items, onScroll, isLoading, hasLoaded }) => {
   }
 
   return (
-    <Container className={cx('pd-list')}>
-      <Row className="mt-3">
-        {items.map((item, index) => (
-          <Item src={item.src} key={item.name} name={item.name} index={index} />
-        ))}
-      </Row>
-      {onScroll && (
-        <Visibility onChange={onScroll}>
-          <BottomPixel />
-        </Visibility>
-      )}
-    </Container>
+    <AnimatePresence>
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <Container className={cx('pd-list')}>
+          <Row className="mt-3">
+            {items.map((item, index) => (
+              <Item src={item.src} key={item.name} name={item.name} index={index} />
+            ))}
+          </Row>
+          {onScroll && (
+            <Visibility onChange={onScroll}>
+              <BottomPixel />
+            </Visibility>
+          )}
+        </Container>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
