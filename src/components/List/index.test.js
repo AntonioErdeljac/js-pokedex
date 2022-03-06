c1c6b431720b5a5e04fda026e86d872507a85cc6
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2022-03-06 23:00:56 +0100

    test: add unit tests for search filter

--- src/components/List/index.test.js
+++ src/components/List/index.test.js
@@ -6,10 +6,11 @@ import List from './index';
 
 const EXPECTED_ITEMS_LENGTH = 2;
 
-const makeProps = ({ items, hasLoaded, isLoading } = {}) => ({
+const makeProps = ({ items, hasLoaded, isLoading, onSearch } = {}) => ({
   items: items || [{ name: 'Lorem' }, { name: 'Ipsum' }],
   hasLoaded: hasLoaded || false,
   isLoading: isLoading || false,
+  onSearch: onSearch || null,
 });
 
 const renderComponent = (props) => {
@@ -28,5 +29,12 @@ describe('List', () => {
 
   it('renders correct number of items', () => {
     expect(wrapper.find('Memo(Item)').length).toEqual(EXPECTED_ITEMS_LENGTH);
+  });
+
+  it('renders search filter conditionally', () => {
+    expect(wrapper.find('FormControl').length).toEqual(0);
+
+    const wrapperWithSearch = renderComponent(makeProps({ onSearch: () => {} }));
+    expect(wrapperWithSearch.find('FormControl').length).toEqual(1);
   });
 });
